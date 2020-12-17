106e9b1c699d30ee4b9286457454580ab6424de9
VeRTeXR <p5vasut27665@gmail.com>
2020-12-17 12:43:26 +0700

    - Add Leantween - Add TextMeshPro - Add Analytic Button

---
+++ Assets/Firebase_Leaderboard/Scripts/AnalyticButtonController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Firebase_Leaderboard.Scripts
+{
+    public class AnalyticButtonController : MonoBehaviour
+    {
+        private Button button; 
+    
+        void Start()
+        {
+            button = GetComponent<Button>();
+            button.onClick.RemoveAllListeners();
+            button.onClick.AddListener(GoToAnalyticPage);
+        }
+
+        private void GoToAnalyticPage()
+        {
+        }
+    }
+}
---
+++ Assets/Firebase_Leaderboard/Scripts/PanelController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using echo17.Signaler.Core;
+using UnityEngine;
+
+namespace Firebase_Leaderboard.Scripts
+{
+    public class PanelController : MonoBehaviour, ISubscriber
+    {
+        [SerializeField] private CanvasGroup analyticPanel;
+        [SerializeField] private CanvasGroup leaderboardPanel;
+
+        private void Awake()
+        {
+            Signaler.Instance.Subscribe<SwitchToAnalytics>(this, ShowAnalyticPage);
+            Signaler.Instance.Subscribe<ShowLeaderboard>(this, ShowLeaderboard);
+        }
+
+        private bool ShowLeaderboard(ShowLeaderboard signal)
+        {
+            return true;
+        }
+
+        private bool ShowAnalyticPage(SwitchToAnalytics signal)
+        {
+            
+                return true;
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+
+    internal struct ShowLeaderboard
+    {
+    }
+
+    internal struct SwitchToAnalytics
+    {
+    }
+}
